ccc19532a12ce99197a95d70696a5265b1732fc0
Anh Nguyen <anhmanchester2k1@gmail.com>
2020-03-03 00:19:10 +0700

    jp-04(account)

--- jp04/Account.java
+++ jp04/Account.java
@@ -5,10 +5,61 @@
  */
 package jp04;
 
+
+
 /**
  *
  * @author Anh
  */
 public class Account {
+    private int id;
+    private Customer customer;
+    private double balance;
+
+    public Account(int id, Customer customer, double balance) {
+        this.id = id;
+        this.customer = customer;
+        this.balance = balance;
+    }
+
+    public Account(int id, Customer customer) {
+        this.id = id;
+        this.customer = customer;
+    }
+    public int getId(){
+        return id;
+    }
+    public Customer getCustomer(){
+        return customer;
+    } 
+    public double getBalance(){
+        return balance;
+    }
+    public void setBalance(double balance){
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" + "id=" + id + ", customer=" + customer + ", balance=" + balance + '}';
+    }
+    
+    public String getCustomerName(){
+        return customer.getName();
+    }
+    public double deposit(double amount){
+         balance = balance + amount;
+         return  balance;
+    }
+    public double withdraw(double amount){
+       if(balance >= amount){
+           return balance = balance - amount;
+       }
+       else{
+               System.out.print("amount withdrawn exceeds the current balance ");
+               }
+        return balance;
+        
+    }
     
 }
